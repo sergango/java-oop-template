f736231afef135bc26e9fc32e8bf111227f3e852
Sergey Bespalov <serg.bespalov@pm.me>
2021-12-28 03:08:09 +0600

    realize save, findByFullName methods

--- src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
+++ src/main/java/com/epam/izh/rd/online/repository/SimpleAuthorRepository.java
@@ -1,4 +1,42 @@
 package com.epam.izh.rd.online.repository;
 
-public class SimpleAuthorRepository {
+import com.epam.izh.rd.online.entity.Author;
+
+import java.util.Arrays;
+
+public class SimpleAuthorRepository implements AuthorRepository {
+    private Author[] authors = new Author[0];
+
+    @Override
+    public boolean save(Author author) {
+        if(findByFullName(author.getName(), author.getLastName()) == null) {
+            authors = Arrays.copyOf(authors, authors.length + 1);
+            authors[authors.length - 1] = author;
+
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public Author findByFullName(String name, String lastname) {
+        for(Author author: authors) {
+            if(name == author.getName() && lastname == author.getLastName()) {
+                return author;
+            }
+        }
+
+        return null;
+    }
+
+    @Override
+    public boolean remove(Author author) {
+        return false;
+    }
+
+    @Override
+    public int count() {
+        return authors.length;
+    }
 }
